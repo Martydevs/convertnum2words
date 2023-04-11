b0a45b743e1be2e84b9bc4cc2cb4f62aff656b3b
Andre Marti <tmdev64@gmail.com>
2023-04-11 15:27:13 +0000

    Revert "Se unificó el acceso de modulos y ejecución de la funcion principal"
    This reverts commit e000b40cf8a0b9f335a1261923e2bfd31a9c4db5.

---
+++ src/utils/units.py
@@ -0,0 +1,46 @@
+unidades = [
+    "cero",
+    "uno",
+    "dos",
+    "tres",
+    "cuatro",
+    "cinco",
+    "seis",
+    "siete",
+    "ocho",
+    "nueve",
+]
+decenas = [
+    "diez",
+    "once",
+    "doce",
+    "trece",
+    "catorce",
+    "quince",
+    "dieciséis",
+    "diecisiete",
+    "dieciocho",
+    "diecinueve",
+]
+decenas2 = [
+    "veinte",
+    "treinta",
+    "cuarenta",
+    "cincuenta",
+    "sesenta",
+    "setenta",
+    "ochenta",
+    "noventa",
+]
+centenas = [
+    "cien",
+    "doscientos",
+    "trescientos",
+    "cuatrocientos",
+    "quinientos",
+    "seiscientos",
+    "setecientos",
+    "ochocientos",
+    "novecientos",
+]
+unidades_millar = ["mil", "un millón"]

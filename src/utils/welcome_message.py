258fd8768389e7635afbfae8103d279280925d2b
Martydevs <tmdev64@gmail.com>
2023-04-10 23:27:33 -0600

    commit

--- src/utils/welcome_message.py
+++ src/utils/welcome_message.py
@@ -7,4 +7,4 @@ init(autoreset=True)
 def welcome_message():
     text = pyfiglet.figlet_format("Number2Word", font="big")
     result = Fore.RED + Style.BRIGHT + text + Style.RESET_ALL
-    return f"{result} \n  Una Aplicacion simple hecha en Python que convierte un numero a palabras. \n  Hecho por: André Martí, Gerardo Arredondo y Said Velazquez \n  Version: 1.0.0 \n  Repositorio: \n"
+    return f"{result} \n  Una Aplicacion simple hecha en Python que convierte un numero a palabras. \n  Hecho por: André Martí, Gerardo Arredondo y Said Velazquez \n  Version: 1.0.0 \n  Repositorio: https://github.com/Martydevs/convertnum2words.git \n"
